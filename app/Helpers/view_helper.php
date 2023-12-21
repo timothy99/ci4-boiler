d1a047c74e1fffa7bc7f4480047d43d615c96fec
bjm1175 <bjm1175@nate.com>
2023-12-21 15:25:58 +0900

    페이지 로딩 헬퍼 추가 및 적용

--- app/Helpers/view_helper.php
+++ app/Helpers/view_helper.php
@@ -27,14 +27,7 @@ function uview($name, $data = array())
     $data["session"] = $session;
     $data["uri"] = $uri;
 
-    $header_file = "usr/include/header";
-    if ($uri == "home/home") { // 홈화면인경우 불러올 스크립트가 바뀜
-        $header_file = "usr/include/header_index";
-    } else {
-        $header_file = "usr/include/header";
-    }
-
-    $header = view($header_file, $data);
+    $header = view("usr/include/header", $data);
     $menu = view("usr/include/menu", $data);
     $view = view($name, $data);
     $footer = view("usr/include/footer", $data);
