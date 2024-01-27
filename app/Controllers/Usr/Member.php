f8d6d6a32d1932464d88e6ab3a1e0e7e68c32d71
timothy99 <joydisk@gmail.com>
2024-01-28 00:06:12 +0900

    우편번호 검색기능 추가

--- app/Controllers/Usr/Member.php
+++ app/Controllers/Usr/Member.php
@@ -6,22 +6,26 @@ use App\Controllers\BaseController;
 
 class Member extends BaseController
 {
+    // 인덱스. 내 정보 보기로 보내기
     public function index()
     {
         header("Location: /member/myinfo");
         exit;
     }
 
+    // 내 정보
     public function myinfo()
     {
         return uview("/usr/member/myinfo");
     }
 
+    // 로그인
     public function login()
     {
         return uview("/usr/member/login");
     }
 
+    // 회원가입
     public function join()
     {
         return uview("/usr/member/join");
--- app/Views/usr/include/menu.php
+++ app/Views/usr/include/menu.php
@@ -1,30 +1,28 @@
-
-
     <body>
         <!-- Spinner Start -->
-        <div id="spinner" class="show w-100 vh-100 bg-white position-fixed translate-middle top-50 start-50  d-flex align-items-center justify-content-center">
+        <div id="spinner" class="w-100 vh-100 bg-white position-fixed translate-middle top-50 start-50 d-flex align-items-center justify-content-center">
             <div class="spinner-grow text-primary" role="status"></div>
         </div>
         <!-- Spinner End -->
 
         <!-- Navbar start -->
-        <div class="container-fluid fixed-top">
+        <div class="container-fluid fixed-top" style="top: 0px;">
             <div class="container topbar bg-primary d-none d-lg-block">
                 <div class="d-flex justify-content-between">
                     <div class="top-info ps-2">
-                        <small class="me-3"><i class="fas fa-map-marker-alt me-2 text-secondary"></i> <a href="#" class="text-white">123 Street, New York</a></small>
-                        <small class="me-3"><i class="fas fa-envelope me-2 text-secondary"></i><a href="#" class="text-white">Email@Example.com</a></small>
+                        <small class="me-3"><i class="fas fa-map-marker-alt me-2 text-secondary"></i> <a href="#" class="text-white">주소</a></small>
+                        <small class="me-3"><i class="fas fa-envelope me-2 text-secondary"></i><a href="#" class="text-white">이메일</a></small>
                     </div>
                     <div class="top-link pe-2">
-                        <a href="#" class="text-white"><small class="text-white mx-2">Privacy Policy</small>/</a>
-                        <a href="#" class="text-white"><small class="text-white mx-2">Terms of Use</small>/</a>
-                        <a href="#" class="text-white"><small class="text-white ms-2">Sales and Refunds</small></a>
+                        <a href="/member/join" class="text-white"><small class="text-white mx-2">회원가입</small>/</a>
+                        <a href="#" class="text-white"><small class="text-white mx-2">로그인</small>/</a>
+                        <a href="#" class="text-white"><small class="text-white ms-2">마이페이지</small></a>
                     </div>
                 </div>
             </div>
             <div class="container px-0">
                 <nav class="navbar navbar-light bg-white navbar-expand-xl">
-                    <a href="index.html" class="navbar-brand"><h1 class="text-primary display-6">Fruitables</h1></a>
+                    <a href="index.html" class="navbar-brand"><h1 class="text-primary display-6">과일가게</h1></a>
                     <button class="navbar-toggler py-2 px-3" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse">
                         <span class="fa fa-bars text-primary"></span>
                     </button>
